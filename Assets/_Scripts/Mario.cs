26a210fcc6975d46faacf308079d6092f4c1d0e4
CharlesJuniorF <161252667+CharlesJuniorF@users.noreply.github.com>
2025-10-16 16:11:32 -0400

    made mario sprite w/ gravity and added collisoins
    some minor things. i will get into the THICK of it tonight

---
+++ Assets/_Scripts/Mario.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mario : MonoBehaviour
+
+{
+    public Rigidbody rb;
+
+    private int Xpos = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if ()
+    }
+}
